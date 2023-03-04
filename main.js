90d98dce318446260c8434162fef8ccb9c974201
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-03-03 19:40:02 -0600

    Add styles, event animations, & header

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 const sectionBtns = document.querySelectorAll(".collBtn");
 const btnArr = Array.from(sectionBtns);
 
+//loop over btn array
 for (let i = 0; i < btnArr.length; i++) {
-  let filteredBtns = btnArr.filter((btn) => btn !== btnArr[i]);
-  console.log(filteredBtns);
-  btnArr[i].addEventListener("click", function (e) {
-    let content = this.nextElementSibling;
-    content.classList.add("active");
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-      content.classList.remove("active");
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-      filteredBtns.forEach(
-        (btn) => (btn.nextElementSibling.style.maxHeight = null)
-      );
-    }
-  });
+    //filter out the current button in btn array
+    let filteredBtns = btnArr.filter((btn) => btn !== btnArr[i]);
+
+    //give current button event listener
+    btnArr[i].addEventListener("click", function (e) {
+        //select collapsed content for current button
+        let content = this.nextElementSibling;
+        //add active class to clicked button
+        content.classList.add("activeContent");
+        this.classList.add("activeBtn")
+
+        //if maxHeight is set
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+            content.classList.remove("activeContent");
+            btnArr[i].classList.remove("activeBtn");
+        } else {
+            //if maxHeight not set, give a maxHeight of contents vertical height
+            content.style.maxHeight = ((content.scrollHeight + 128)+ "px");
+            //remove maxHeight and active class from buttons NOT clicked on 
+            filteredBtns.forEach(
+                (btn) => (btn.nextElementSibling.style.maxHeight = null, btn.nextElementSibling.classList.remove("activeContent"), btn.classList.remove("activeBtn")));
+        }
+    });
 }
