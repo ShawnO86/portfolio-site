ede5c3d3cffe6e2baeb7542403e834704d4a0dfd
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-03-15 11:59:08 -0500

    optimized a bit more

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const [aboutBtn, projectBtn, contactBtn] = btnArr;
 const smallFont = 'clamp(0.75rem, 1.5vw, 1rem)';
 const largeFont = 'clamp(1.25rem, 2vw, 2.25rem)';
 
+const getNextElement = (button) => button.nextElementSibling;
+
 setTimeout(() => {
     aboutBtn.classList.add('activeBtn');
     projectBtn.style.fontSize = smallFont
@@ -18,43 +20,38 @@ setTimeout(() => {
 //loop over btn array
 for (let i = 0; i < btnArr.length; i++) {
     //filter out the current button in btn array
-    let filteredBtns = btnArr.filter((btn) => btn !== btnArr[i]);
+    const filteredBtns = btnArr.filter((btn) => btn !== btnArr[i]);
     //give current button event listener
     btnArr[i].addEventListener('click', function () {
         //select collapsed content for current button
-        let content = this.nextElementSibling;
+        let content = getNextElement(this);
         //add active class to clicked button       
-
         content.classList.add('activeContent');
         this.classList.add('activeBtn');
-
         filteredBtns.forEach((btn) => {
-                btn.nextElementSibling.style.maxHeight = null;
-                btn.classList.remove('activeBtn');
-                btn.nextElementSibling.classList.remove('activeContent');
-                btn.style.fontSize = largeFont;
+            getNextElement(btn).style.maxHeight = null;
+            btn.classList.remove('activeBtn');
+            getNextElement(btn).classList.remove('activeContent');
+            btn.style.fontSize = largeFont;
         });
-
         //if maxHeight is set
         if (content.style.maxHeight) {
-                content.style.maxHeight = null;
-                this.classList.remove('activeBtn');
-                content.classList.remove('activeContent');
-                this.style.color = smallFont
-
+            content.style.maxHeight = null;
+            this.classList.remove('activeBtn');
+            content.classList.remove('activeContent');
+            this.style.color = smallFont
         } else {
             //if maxHeight not set, give a maxHeight of contents vertical height
             //timeout allows for collapse animation to happen first
             setTimeout(() => {
                 content.style.maxHeight = content.scrollHeight + 'px';
                 this.style.fontSize = largeFont;
-            }, 150)
-
+            }, 300)
             //remove maxHeight and active class from buttons NOT clicked on 
             filteredBtns.forEach((btn) => {
-                btn.nextElementSibling.style.maxHeight = null;
+                getNextElement(btn).style.maxHeight = null;
                 btn.classList.remove('activeBtn');
-                btn.nextElementSibling.classList.remove('activeContent');
+                getNextElement(btn).classList.remove('activeContent');
                 btn.style.fontSize = smallFont;
             });
         }
