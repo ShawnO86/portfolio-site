8853c50a660f9a018e6112e063a72d5971ca555d
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-03-15 04:15:00 -0500

    In browser prototyping + font size resize to container

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const sectionBtns = document.querySelectorAll(".collBtn");
-const contentArea = document.querySelectorAll(".content"); 
+const contentArea = document.querySelectorAll(".content");
 const btnArr = Array.from(sectionBtns);
 const aboutBtn = document.getElementById("aboutBtn");
 const aboutCont = document.getElementById("About");
+const projectBtn = document.getElementById("projectBtn");
+const contactBtn = document.getElementById("contactBtn");
+
+const smallFont = 'clamp(0.75rem, 1.5vw, 1rem)';
+const largeFont = 'clamp(1.25rem, 2vw, 2.25rem)';
 
 setTimeout(() => {
     aboutBtn.classList.add("activeBtn")
+    projectBtn.style.fontSize = smallFont
+    contactBtn.style.fontSize = smallFont
     aboutCont.classList.add("activeContent")
     setTimeout(() => {
-    aboutCont.style.maxHeight = "100%";
-    aboutCont.style.maxWidth = "100%";
-    }, 100)
+        aboutCont.style.maxHeight = (aboutCont.scrollHeight + 128) + "px";
+    }, 150)
 }, 1000)
 
 
@@ -23,31 +29,38 @@ for (let i = 0; i < btnArr.length; i++) {
     btnArr[i].addEventListener("click", function () {
         //select collapsed content for current button
         let content = this.nextElementSibling;
-        //add active class to clicked button
+        //add active class to clicked button       
         content.classList.add("activeContent");
-        this.classList.add("activeBtn");
+        this.classList.add("activeBtn")
+
+        filteredBtns.forEach((btn) => {
+            (btn.nextElementSibling.style.maxHeight = null,
+                btn.classList.remove("activeBtn")),
+                btn.nextElementSibling.classList.remove("activeContent"),
+                btn.style.fontSize = largeFont;
+        });
 
         //if maxHeight is set
         if (content.style.maxHeight) {
                 content.style.maxHeight = null;
-                content.style.maxWidth = null
                 content.classList.remove("activeContent");
-                btnArr[i].classList.remove("activeBtn");
+                this.classList.remove("activeBtn");
+                this.style.color = smallFont
         } else {
             //if maxHeight not set, give a maxHeight of contents vertical height
             //timeout allows for collapse animation to happen first
             setTimeout(() => {
-                content.style.maxHeight = ((content.scrollHeight + 128) + "px");
-                content.style.maxWidth = "100%";
-            }, 250)
+                content.style.maxHeight = content.scrollHeight + "px";
+                this.style.fontSize = largeFont;
+            }, 100)
 
             //remove maxHeight and active class from buttons NOT clicked on 
             filteredBtns.forEach((btn) => {
-                (btn.nextElementSibling.style.maxHeight = null,
-                    btn.nextElementSibling.style.maxWidth = null,
-                    btn.classList.remove("activeBtn")),
-                    btn.nextElementSibling.classList.remove("activeContent")
-                });
+                btn.nextElementSibling.style.maxHeight = null,
+                    btn.classList.remove("activeBtn"),
+                    btn.nextElementSibling.classList.remove("activeContent"),
+                    btn.style.fontSize = smallFont
+            });
         }
     });
 };
