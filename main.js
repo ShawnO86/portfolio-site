b797c621b65d53866f93af3d10643a8f47be8966
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-03-26 14:12:58 -0500

    buttons tabbable & enter listener & better visiblity

--- main.js
+++ main.js
@@ -11,41 +11,53 @@ const getNextElement = (button) => button.nextElementSibling;
 
 //loop over btn array
 for (let i = 0; i < btnArr.length; i++) {
-    //filter out the current button in btn array
-    const filteredBtns = btnArr.filter((btn) => btn !== btnArr[i]);
     //give current button event listener
     btnArr[i].addEventListener('click', function () {
-        //select collapsed content for current button
-        let content = getNextElement(this);
-        //add active class to clicked button       
-        content.classList.add('activeContent');
-        this.classList.add('activeBtn');
-        //remove active class from button not clicked and give large font to all other
-        filteredBtns.forEach((btn) => {
-            getNextElement(btn).style.maxHeight = null;
-            btn.classList.remove('activeBtn');
-            getNextElement(btn).classList.remove('activeContent');
-            btn.style.fontSize = largeFont;
-        });
-        //if maxHeight is set on clicked button, collapse content and remove active classes
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-            this.classList.remove('activeBtn');
-            content.classList.remove('activeContent');
-        } else {
-            //if maxHeight not set, give a maxHeight of the contents scroll height
-            setTimeout(() => {
-                content.style.maxHeight = (content.scrollHeight + 200) + 'px';
-                this.style.fontSize = largeFont;
-            }, 500)
-
-            //remove maxHeight and active class from buttons NOT clicked on 
-            filteredBtns.forEach((btn) => {
-                getNextElement(btn).style.maxHeight = null;
-                btn.classList.remove('activeBtn');
-                getNextElement(btn).classList.remove('activeContent');
-                btn.style.fontSize = smallFont;
-            });
+        //select collapsed content for current button on left click
+       openContent(btnArr[i])
+    });
+    btnArr[i].addEventListener('keypress', function (e) {
+        //select collapsed content for current button on enter press
+        if(e.key === "Enter") {
+            openContent(btnArr[i])
         }
+ 
     });
 };
+
+function openContent(button) {
+        //filter out the current button in btn array
+        const filteredBtns = btnArr.filter((btn) => btn !== button);
+      //select collapsed content for current button
+      let content = getNextElement(button);
+      //add active class to clicked button       
+      content.classList.add('activeContent');
+      button.classList.add('activeBtn');
+      //remove active class from button not clicked and give large font to all other
+      filteredBtns.forEach((btn) => {
+          getNextElement(btn).style.maxHeight = null;
+          btn.classList.remove('activeBtn');
+          getNextElement(btn).classList.remove('activeContent');
+          btn.style.fontSize = largeFont;
+      });
+      //if maxHeight is set on clicked button, collapse content and remove active classes
+      if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+          button.classList.remove('activeBtn');
+          content.classList.remove('activeContent');
+      } else {
+          //if maxHeight not set, give a maxHeight of the contents scroll height
+          setTimeout(() => {
+              content.style.maxHeight = (content.scrollHeight + 200) + 'px';
+              button.style.fontSize = largeFont;
+          }, 500)
+
+          //remove maxHeight and active class from buttons NOT clicked on 
+          filteredBtns.forEach((btn) => {
+              getNextElement(btn).style.maxHeight = null;
+              btn.classList.remove('activeBtn');
+              getNextElement(btn).classList.remove('activeContent');
+              btn.style.fontSize = smallFont;
+          });
+      }
+}
