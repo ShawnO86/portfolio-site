8f423f513d176f56500088e7691fb69941c8cd10
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-04-05 14:07:35 -0500

    accessibility/UX improve

--- main.js
+++ main.js
@@ -5,29 +5,69 @@ const reachOut = document.getElementById('reachOut');
 const smallFont = 'clamp(0.75rem, 1.5vw, 1rem)';
 const largeFont = 'clamp(1.25rem, 2vw, 2.25rem)';
 const getNextElement = (button) => button.nextElementSibling;
+const zoomedMedia = window.matchMedia("(max-height: 20rem)");
 
-//Open about me section on initial load -- after 500ms
-setTimeout(() => {
-    openContent(btnArr[0]);
-}, 500)
+if (zoomedMedia.matches) {
+    zoomed400Xlayout();
+} else {
+    normalLayout();
+    //Open about me section on initial load -- after 500ms -- if its not already open
+    if (!getNextElement(btnArr[0]).style.maxHeight) {
+        setTimeout(() => {
+            openContent(btnArr[0]);
+        }, 500);
+    };
+};
 
-reachOut.addEventListener('click', () => {
-    openContent(btnArr[2]);
-}) 
+window.addEventListener("resize", () => {
+    if (zoomedMedia.matches) {
+        zoomed400Xlayout();
+    } else {
+        for (let i = 0; i < btnArr.length; i++) {
+            btnArr[i].classList.remove('activeBtn');
+            btnArr[i].firstElementChild.setAttribute("aria-expanded", false);
+            getNextElement(btnArr[i]).classList.remove('activeContent');
+            getNextElement(btnArr[i]).setAttribute('aria-hidden', true);
+            getNextElement(btnArr[i]).style.maxHeight = null;
+            btnArr[i].style.fontSize = largeFont;
+        };
+        if (getNextElement(btnArr[0]).style.maxHeight) {
+            setTimeout(() => {
+                openContent(btnArr[0]);
+            }, 500);
+        };
+    };
+});
 
-reachOut.addEventListener('keypress', (e) => {
-    if(e.key == 'enter') {
+function zoomed400Xlayout() {
+    for (let i = 0; i < btnArr.length; i++) {
+        btnArr[i].classList.add('activeBtn');
+        btnArr[i].firstElementChild.setAttribute("aria-expanded", true);
+        getNextElement(btnArr[i]).classList.add('activeContent');
+        getNextElement(btnArr[i]).setAttribute('aria-hidden', false);
+        getNextElement(btnArr[i]).style.maxHeight = (getNextElement(btnArr[i]).scrollHeight + 300) + 'px';
+    };
+};
+
+function normalLayout() {
+    reachOut.addEventListener('click', () => {
         openContent(btnArr[2]);
-    }
-}) 
-
-//loop over btn array to set listener to each accordian button
-for (let i = 0; i < btnArr.length; i++) {
-    //set initial font size
-    btnArr[i].style.fontSize = largeFont;
-    btnArr[i].addEventListener('click', () => {
-        openContent(btnArr[i]);
     });
+
+    reachOut.addEventListener('keypress', (e) => {
+        if (e.key == 'enter') {
+            openContent(btnArr[2]);
+        };
+    });
+
+    //loop over btn array to set listener to each accordian button
+    for (let i = 0; i < btnArr.length; i++) {
+        //set initial font size
+        btnArr[i].style.fontSize = largeFont;
+        btnArr[i].addEventListener('click', () => {
+            openContent(btnArr[i]);
+        });
+    };
 };
 
 function openContent(button) {
@@ -38,31 +78,31 @@ function openContent(button) {
     //add active class to clicked button       
     content.classList.add('activeContent');
     button.classList.add('activeBtn');
-    button.firstElementChild.setAttribute("aria-expanded", true)
-    content.setAttribute('aria-hidden', false)
+    button.firstElementChild.setAttribute("aria-expanded", true);
+    content.setAttribute('aria-hidden', false);
     //remove active class from button not clicked and give large font to all other
     filteredBtns.forEach((btn) => {
         getNextElement(btn).style.maxHeight = null;
         btn.classList.remove('activeBtn');
         getNextElement(btn).classList.remove('activeContent');
-        btn.firstElementChild.setAttribute("aria-expanded", false)
+        btn.firstElementChild.setAttribute("aria-expanded", false);
         btn.style.fontSize = largeFont;
-        getNextElement(btn).setAttribute('aria-hidden', true)
+        getNextElement(btn).setAttribute('aria-hidden', true);
     });
     //if maxHeight is set on clicked button, collapse content and remove active classes
     if (content.style.maxHeight) {
         content.style.maxHeight = null;
         button.classList.remove('activeBtn');
         content.classList.remove('activeContent');
-        content.setAttribute('aria-hidden', true)
-        button.firstElementChild.setAttribute("aria-expanded", false)
+        content.setAttribute('aria-hidden', true);
+        button.firstElementChild.setAttribute("aria-expanded", false);
     } else {
         //if maxHeight not set, give a maxHeight of the contents scroll height
         setTimeout(() => {
-            content.style.maxHeight = (content.scrollHeight + 200) + 'px';
+            content.style.maxHeight = (content.scrollHeight + 300) + 'px';
             button.style.fontSize = largeFont;
-            button.firstElementChild.setAttribute("aria-expanded", true)
-            content.setAttribute('aria-hidden', false)
+            button.firstElementChild.setAttribute("aria-expanded", true);
+            content.setAttribute('aria-hidden', false);
         }, 400)
         //remove maxHeight and active class from buttons NOT clicked on 
         filteredBtns.forEach((btn) => {
@@ -70,8 +110,8 @@ function openContent(button) {
             btn.classList.remove('activeBtn');
             getNextElement(btn).classList.remove('activeContent');
             btn.style.fontSize = smallFont;
-            btn.firstElementChild.setAttribute("aria-expanded", false)
-            getNextElement(btn).setAttribute('aria-hidden', true)
+            btn.firstElementChild.setAttribute("aria-expanded", false);
+            getNextElement(btn).setAttribute('aria-hidden', true);
         });
-    }
-}
+    };
+};
