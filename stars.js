45e515ea9d3cac1198838485ffa5727840d7af85
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-05-20 13:35:47 -0500

    More efficent with less DOM queries.

--- stars.js
+++ stars.js
@@ -4,28 +4,23 @@ function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
+const fragment = document.createDocumentFragment();
 function generateStarField(low, high) {
-
     for (let i = 0; i < 100; i++) {
-        const star = document.createElement("span");
+        const star = document.createElement("div");
+        const starSize = randomNum(1, 3);
+        const inc_length = randomNum(low, high);
+        const inc_delay = randomNum(1, 4);
+
         star.classList.add("star");
-        space.appendChild(star);
-    }
-
-    const allStars = document.querySelectorAll(".star");
-
-    allStars.forEach(starEl => {
-        const top = randomNum(0, 100);
-        const left = randomNum(0, 100);
-        const size = randomNum(1, 3);
-        const inc = randomNum(low, high);
-        const inc_2 = randomNum(1, 4);
-        starEl.style.top = top + '%';
-        starEl.style.left = left + '%';
-        starEl.style.width = size + 'px';
-        starEl.style.height = size + 'px';
-        starEl.style.animation = 'twinkle ' + inc + 's ' + inc_2 + 's infinite';
-    });
+        star.style.top = randomNum(0, 100) + "%";
+        star.style.left = randomNum(0, 100) + "%";
+        star.style.width = starSize + "px";
+        star.style.height = starSize + "px";
+        star.style.animation = `twinkle ${inc_length}s ${inc_delay}s infinite`;
+        fragment.appendChild(star);
+    };
+    space.appendChild(fragment);
 };
 
 generateStarField(4, 10);
