1a483412dad64881150e8aa582b7d572d47ba383
Shawn <99354351+ShawnO86@users.noreply.github.com>
2023-05-23 01:29:01 -0500

    size var

--- stars.js
+++ stars.js
@@ -8,7 +8,7 @@ const fragment = document.createDocumentFragment();
 function generateStarField(low, high) {
     for (let i = 0; i < 100; i++) {
         const star = document.createElement("div");
-        const starSize = randomNum(1, 3);
+        const starSize = randomNum(1, 3) + "px";
         const inc_length = randomNum(low, high);
         const inc_delay = randomNum(1, 4);
         const position = randomNum(0, 100) + "%";
@@ -16,8 +16,8 @@ function generateStarField(low, high) {
         star.classList.add("star");
         star.style.top = position;
         star.style.left = position;
-        star.style.width = starSize + "px";
-        star.style.height = starSize + "px";
+        star.style.width = starSize;
+        star.style.height = starSize;
         star.style.animation = `twinkle ${inc_length}s ${inc_delay}s infinite`;
         fragment.appendChild(star);
     };
